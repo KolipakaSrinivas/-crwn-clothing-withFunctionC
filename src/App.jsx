376c59be72696f115cd2016ff1cbfa7809ba8487
Srinivas <kolipakasrinivas456@gmail.com>
2023-04-04 07:40:54 +0530

    add header component but crown logo svg not working

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import './app.css'
 
 import HomePage from "./pages/HomePage/HomePage.component"
 import ShopPage from "./pages/shop/shop-page.component"
-
-
+import Header from './components/Header/Heade.component'
 
 
 
@@ -14,6 +13,7 @@ function App() {
   return (
 
     <div className="App">
+      <Header/>
       <Routes>
         <Route path="/" Component={HomePage}/>
         <Route path="/shop" Component={ShopPage}/>
---
+++ src/components/Header/Heade.component.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import React from 'react'
+// import { ReactComponent as Logo} from "../../assets/rown.svg";
+
+import './header.style.component.css'
+
+
+
+function Header() {
+    return <div className="header">
+                 <Link to='/' className="className='logo-container'">
+                {/* <Logo className='logo'/> */}
+                </Link>
+             <div className="options">
+                <Link className="option" to='/shop'>Shop</Link>
+                <Link  className="option">Counts</Link>
+             </div>
+           </div>
+}
+
+export default Header
